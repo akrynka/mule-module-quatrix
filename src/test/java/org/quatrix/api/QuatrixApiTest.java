ac91e7ba9a118af8c3aa21183bdc61e71a9045ae
Alexey Krynka <alexeykrynka@gmail.com>
2018-06-15 16:01:13 +0300

    Test copy files api operation

--- src/test/java/org/quatrix/api/QuatrixApiTest.java
+++ src/test/java/org/quatrix/api/QuatrixApiTest.java
@@ -4,12 +4,15 @@ import io.swagger.client.ApiClient;
 import io.swagger.client.ApiException;
 import io.swagger.client.api.AuthApi;
 import io.swagger.client.api.FileApi;
+import io.swagger.client.model.CopyMoveFilesReq;
+import io.swagger.client.model.JobResp;
 import io.swagger.client.model.SessionLoginResp;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -41,5 +44,20 @@ public class QuatrixApiTest {
 
         Mockito.verify(executorService).scheduleAtFixedRate(
                 Mockito.any(Runnable.class), Mockito.any(Long.class), Mockito.any(Long.class), Mockito.any(TimeUnit.class));
+    }
+
+    @Test
+    public void testFileCopy() throws QuatrixApiException, ApiException {
+        final UUID testId = UUID.randomUUID();
+        final UUID testTarget = UUID.randomUUID();
+        final CopyMoveFilesReq req = new CopyMoveFilesReq();
+
+        req.setIds(Collections.singletonList(testId));
+        req.setTarget(testTarget);
+
+        Mockito.when(fileApi.fileCopyPost(req)).thenReturn(new JobResp().jobId(testId));
+        JobResp response = api.copyFiles(req);
+
+        Assert.assertEquals(testId, response.getJobId());
     }
 }
