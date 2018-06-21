8d560670d0ffbef25f3458107c558c201773552e
alexeykrynka <alexeykrynka@gmail.com>
2018-06-21 12:20:37 +0300

    Implemented func test for deleteFiles api operation

--- src/test/java/org/quatrix/automation/testcases/FileDeleteTestCase.java
+++ src/test/java/org/quatrix/automation/testcases/FileDeleteTestCase.java
@@ -6,7 +6,7 @@ import org.junit.experimental.categories.Category;
 import org.mule.modules.tests.ConnectorTestUtils;
 import org.quatrix.automation.QuatrixParentTestCase;
 import org.quatrix.automation.SmokeTests;
-import org.quatrix.model.FileMetadata;
+import org.quatrix.model.FileIds;
 import org.quatrix.model.FileRenameResult;
 
 import java.util.UUID;
@@ -14,22 +14,22 @@ import java.util.UUID;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
-public class FileRenameTestCase extends QuatrixParentTestCase {
+public class FileDeleteTestCase extends QuatrixParentTestCase {
 
     //TODO(ringil): replace with upload flow
     @Before
     public void setUp() throws Exception {
-        initializeTestRunMessage("renameFileTestData");
-        upsertOnTestRunMessage("uuid", UUID.fromString("c87c1171-c655-49b7-a1b6-9da056095d22"));
+        initializeTestRunMessage("deleteFilesTestData");
+        upsertOnTestRunMessage("ids", UUID.fromString("4c1405b9-9c82-4b85-9a9c-993029e6706c"));
     }
 
     @Category(SmokeTests.class)
     @Test
     public void testRenameFile() {
         try {
-            FileRenameResult fileRenameResult = runFlowAndGetPayload("rename-file");
-            assertNotNull(fileRenameResult);
-            assertNotNull(fileRenameResult.getName());
+            FileIds fileIds = runFlowAndGetPayload("delete-files");
+            assertNotNull(fileIds);
+            assertNotNull(fileIds.getIds().get(0));
         } catch (Exception e) {
             fail(ConnectorTestUtils.getStackTrace(e));
         }
